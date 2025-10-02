e2bbb710b2ee6363309b0af98e738de1cbd7c2bb
Amit Mahto <amitextinct@gmail.com>
2025-10-02 21:08:53 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <>
+            <div className='hero'>
+                <h1>Hi, I'm Amit Mahto</h1>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css';
+export default function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Education</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
+  );
+}
